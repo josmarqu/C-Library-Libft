51a8bd6f8cfd8c918dcb5473b8ade43632ced510
Jose Marquez montoro <josmarqu@c3r4s1.42madrid.com>
2023-01-21 18:41:34 +0100

    strlcat function reimplemented

---
+++ functions/ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josmarqu <josmarqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/21 16:25:23 by josmarqu          #+#    #+#             */
+/*   Updated: 2023/01/21 18:40:57 by josmarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(const char *s)
+{
+	int	c;
+
+	c = 0;
+	while (s[c])
+		c++;
+	return (c);
+}
+
+int	ft_strlcat(char *dest, const char *src, int destsize)
+{
+	int	c;
+	int	len;
+
+	c = ft_strlen(dest);
+	len = c + ft_strlen(src);
+	if (len >= destsize)
+		return (len);
+	dest = dest + c;
+	while (c++ < destsize)
+		*dest++ = *src++;
+	return (len);
+}
