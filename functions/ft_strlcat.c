a986f873616e51882a590d1ece8e0e0436a44101
Jose Marquez montoro <josmarqu@c3r4s1.42madrid.com>
2023-01-21 18:58:31 +0100

    norminette ok

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -6,19 +6,11 @@
 /*   By: josmarqu <josmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:25:23 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/01/21 18:51:57 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/01/21 18:58:13 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(const char *s)
-{
-	int	c;
-
-	c = 0;
-	while (s[c])
-		c++;
-	return (c);
-}
+int	ft_strlen(const char *s);
 
 int	ft_strlcat(char *dest, const char *src, int destsize)
 {
