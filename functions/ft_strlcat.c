e9ed4a2afecd2cc2680a86d1e1f40323e624ba55
Jose Marquez montoro <josmarqu@c3r4s1.42madrid.com>
2023-01-21 19:01:22 +0100

    fixed

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: josmarqu <josmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:25:23 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/01/21 18:58:13 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/01/21 19:01:06 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ int	ft_strlcat(char *dest, const char *src, int destsize)
 	dest = dest + c;
 	while (c++ < destsize)
 		*dest++ = *src++;
+	*dest = '\0';
 	return (len);
-	dest[c] = 0;
 }
