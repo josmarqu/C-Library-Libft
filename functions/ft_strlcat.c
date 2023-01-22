dc9879c723ec379c1eef2613a5b26a2693300f3f
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-01-22 14:50:05 +0100

    test

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -3,14 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josmarqu <josmarqu@student.42.fr>          +#+  +:+       +#+        */
+/*   By: josepc <josepc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:25:23 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/01/21 19:31:15 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/01/22 12:17:42 by josepc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(const char *s);
+int	ft_strlen(const char *s)
+{
+	int	c;
+
+	c = 0;
+	while (*s++)
+		c++;
+	return (c);
+}
 
 int	ft_strlcat(char *dest, const char *src, int destsize)
 {
@@ -21,9 +29,25 @@ int	ft_strlcat(char *dest, const char *src, int destsize)
 	len = c + ft_strlen(src);
 	if (len >= destsize)
 		return (destsize + ft_strlen(src));
-	dest = dest + c;
+	dest += c;
 	while (c++ < destsize)
 		*dest++ = *src++;
 	*dest = 0;
 	return (len);
+}
+
+// Test ft_strlcat
+
+#include <bsd/stdio.h>
+#include <bsd/string.h>
+
+int	 main(void)
+{
+	char dest[20] = "Hello";
+	char src[20] = "World";
+	int destsize = 7;
+
+	printf("strlcat: %ld\n", strlcat(dest, src, destsize));
+	printf("dest: %s\n", dest);
+	return (0);
 }
