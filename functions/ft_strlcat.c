a9b7f75bb601a3d8036e4483090beaa4a4be184c
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-01-22 16:34:21 +0100

    simplified

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: josepc <josepc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:25:23 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/01/22 16:32:46 by josepc           ###   ########.fr       */
+/*   Updated: 2023/01/22 16:33:55 by josepc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,7 @@ int	ft_strlcat(char *dest, const char *src, int destsize)
 
 	c = ft_strlen(dest);
 	if (destsize < c)
-	{
 		return (c + destsize);
-	}
 	len = c + ft_strlen(src);
 	dest += c;
 	while (c++ < destsize - 1)
