0e6a93f17b261802f0ba76265a405e56f4fd305d
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-01-22 16:33:17 +0100

    strlcat reimplementation fixed

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -6,19 +6,9 @@
 /*   By: josepc <josepc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:25:23 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/01/22 12:17:42 by josepc           ###   ########.fr       */
+/*   Updated: 2023/01/22 16:32:46 by josepc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-int	ft_strlen(const char *s)
-{
-	int	c;
-
-	c = 0;
-	while (*s++)
-		c++;
-	return (c);
-}
 
 int	ft_strlcat(char *dest, const char *src, int destsize)
 {
@@ -26,28 +16,14 @@ int	ft_strlcat(char *dest, const char *src, int destsize)
 	int	len;
 
 	c = ft_strlen(dest);
+	if (destsize < c)
+	{
+		return (c + destsize);
+	}
 	len = c + ft_strlen(src);
-	if (len >= destsize)
-		return (destsize + ft_strlen(src));
 	dest += c;
-	while (c++ < destsize)
+	while (c++ < destsize - 1)
 		*dest++ = *src++;
 	*dest = 0;
 	return (len);
-}
-
-// Test ft_strlcat
-
-#include <bsd/stdio.h>
-#include <bsd/string.h>
-
-int	 main(void)
-{
-	char dest[20] = "Hello";
-	char src[20] = "World";
-	int destsize = 7;
-
-	printf("strlcat: %ld\n", strlcat(dest, src, destsize));
-	printf("dest: %s\n", dest);
-	return (0);
 }
