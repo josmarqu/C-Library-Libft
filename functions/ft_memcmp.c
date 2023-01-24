a3bc2bd297a1621d03cba664899335a0f45d2743
JoseLaptop <josemarquezmontoro@hotmail.com>
2023-01-24 11:25:30 +0100

    memchr function reimplemented

--- functions/ft_memcmp.c
+++ functions/ft_memcmp.c
@@ -1,33 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: joselaptop <joselaptop@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/23 13:17:28 by joselaptop        #+#    #+#             */
-/*   Updated: 2023/01/24 11:23:45 by joselaptop       ###   ########.fr       */
+/*   Created: 2023/01/24 09:29:27 by joselaptop        #+#    #+#             */
+/*   Updated: 2023/01/24 09:38:57 by joselaptop       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memchr(const void *s, int c, int n)
-{
-	unsigned char	*scopy;
+/*
+	int		ft_memcmp(const void *s1, const void *s2, int n)
+	{
 
-	scopy = (unsigned char *) s;
-	while (n--)
-		if (scopy[n] == c)
-			return (scopy + n);
-	return (0);
-}
+	}
+*/
 
-// test memchr
+// test memcmp
 #include <bsd/stdio.h>
 #include <bsd/string.h>
 
 int	main(void)
 {
 	char str1[] = "Hello";
+	char str2[] = "Hillo";
 
-	printf("%p\n", memchr(str1, 'l', 1));
+	printf("%d\n", strncmp(str1, str2, 1));
 }
