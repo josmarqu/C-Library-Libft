79b9dd28cc4bf23ffd4dea03cfef7cba6f150623
Jose Marquez montoro <josmarqu@c3r13s3.42madrid.com>
2023-01-24 16:27:12 +0100

    strnstr function reimplemented

--- functions/ft_strnstr.c
+++ functions/ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joselaptop <joselaptop@student.42.fr>      +#+  +:+       +#+        */
+/*   By: josmarqu <josmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 13:08:07 by joselaptop        #+#    #+#             */
-/*   Updated: 2023/01/24 13:51:40 by joselaptop       ###   ########.fr       */
+/*   Updated: 2023/01/24 16:26:38 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,25 @@ int	ft_strlen(const char *str);
 
 char	*ft_strnstr(const char *haystack, const char *needle, int len)
 {
-	int	ch_counter;
-	int s_len;
-	unsigned char *s;
-	unsigned char *f;
+	int		ch_counter;
+	int		f_len;
+	char	*s;
+	char	*f;
 
 	ch_counter = 0;
 	f_len = ft_strlen(needle);
-	s = (unsigned char *) haystack;
-	f = (unsigned char *) needle;
-
-	while (--len)
+	s = (char *) haystack;
+	f = (char *) needle;
+	while (len-- && ch_counter != f_len)
 	{
 		if (*s == *f)
 		{
 			ch_counter += 1;
-			*f++;
+			f++;
 		}
-		if (ch_counter == f_len)
-		{
-			return (s -  ch_counter)
-		}
-			*s++;
+			s++;
 	}
-
+	if (ch_counter == f_len)
+		return (s - ch_counter);
+	return (0);
 }
-
-
-//Test strnstr function
-
-#include <bsd/stdio.h>
-#include <bsd/string.h>
-
-int	main(void)
-{
-	char *str = "Hello World";
-	char *find = "Wor";
-	int len = 9;
-
-	printf("strnstr: %s\n", strnstr(str, find, len));
-}
-
-
