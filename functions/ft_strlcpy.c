87f16fcc48e0f4374827d4f0d048b2c4c2ddd781
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-01-25 18:39:22 +0100

    function reimplemented fixed

--- functions/ft_strlcpy.c
+++ functions/ft_strlcpy.c
@@ -6,23 +6,45 @@
 /*   By: josepc <josepc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 18:23:15 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/01/25 18:31:10 by josepc           ###   ########.fr       */
+/*   Updated: 2023/01/25 18:38:45 by josepc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(const char *s);
+int	ft_strlen(const char *s)
+{
+	int	c;
+
+	c = 0;
+	while (s[c])
+		c++;
+	return (c);
+}
 
 int	ft_strlcpy(char *dest, const char *src, int destsize)
 {
-	int	c;
-	int	n;
+	int	r;
+	int i;
 
-	c = ft_strlen(src);
-	n = c;
-	if (destsize > c)
-		c = destsize;
-	while (c-- > 1)
+	r = ft_strlen(src);
+	i = destsize;
+	while (i-- > 1)
 		*dest++ = *src++;
 	*dest = 0;
-	return (n);
+	return (r);
+}
+
+// test ft_strlcpy
+#include <bsd/stdio.h>
+#include <bsd/string.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char	*str;
+	char	*str2;
+
+	str = "Hello World!";
+	str2 = malloc(ft_strlen(str) * sizeof(char));
+	printf("strlcpy: %d\n", ft_strlcpy(str2, str, 13));
+	printf("str2: %s\n", str2);
 }
