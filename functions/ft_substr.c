9fdc9f698841a8140b3d661530f0739bcd6c4d71
JoseLaptop <josemarquezmontoro@hotmail.com>
2023-01-30 12:07:37 +0100

    function refactorized

--- functions/ft_substr.c
+++ functions/ft_substr.c
@@ -6,32 +6,15 @@
 /*   By: joselaptop <joselaptop@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 11:06:06 by joselaptop        #+#    #+#             */
-/*   Updated: 2023/01/30 12:05:41 by joselaptop       ###   ########.fr       */
+/*   Updated: 2023/01/30 12:07:23 by joselaptop       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_strlen(const char *s)
-{
-	unsigned int	i;
+int	ft_strlen(const char *s);
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-int	ft_strlcpy(char *dest, const char *src, int destsize)
-{
-	unsigned int	src_len;
-
-	src_len = ft_strlen(src);
-	while (destsize-- > 1)
-		*dest++ = *src++;
-	*dest = 0;
-	return (src_len);
-}	
+int	ft_strlcpy(char *dest, const char *src, int destsize);
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -43,18 +26,4 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	s += start;
 	ft_strlcpy(sub_s, s, len + 1);
 	return (sub_s);
-}
-
-// test substr
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	*str = "Hello World!";
-	char	*sub_str;
-
-	sub_str = ft_substr(str, 6, 5);
-	printf("%s", sub_str);
-	return (0);
 }
