e7dc9022eec055f35bbe91d5419cbaac56995c97
JoseLaptop <josemarquezmontoro@hotmail.com>
2023-01-30 13:05:29 +0100

    function fixed

--- functions/ft_strlcat.c
+++ functions/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josepc <josepc@student.42.fr>              +#+  +:+       +#+        */
+/*   By: joselaptop <joselaptop@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 16:25:23 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/01/28 16:38:09 by josepc           ###   ########.fr       */
+/*   Updated: 2023/01/30 13:05:09 by joselaptop       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_strlen(const char *s);
 
 int	ft_strlcat(char *dest, const char *src, int destsize)
 {
-	unsigned int	dest_len;
+	int				dest_len;
 	unsigned int	len;
 
 	dest_len = ft_strlen(dest);
