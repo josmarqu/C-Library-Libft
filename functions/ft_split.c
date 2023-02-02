722f297a043e21fb095034633b782241463b64ef
Jose Marquez montoro <josmarqu@c1r15s5.42madrid.com>
2023-02-02 18:30:05 +0100

    function implemented but norminette and molinette no verified

---
+++ functions/ft_split.c
@@ -0,0 +1,138 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josmarqu <josmarqu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/02 15:23:15 by josmarqu          #+#    #+#             */
+/*   Updated: 2023/02/02 18:27:55 by josmarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen(const char *s)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_strlcpy(char *dest, const char *src, int destsize)
+{
+	unsigned int	src_len;
+
+	src_len = ft_strlen(src);
+	while (destsize-- > 1)
+		*dest++ = *src++;
+	*dest = 0;
+	return (src_len);
+}	
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub_s;
+
+	sub_s = malloc(len * sizeof(char));
+	if (!sub_s)
+		return (NULL);
+	s += start;
+	ft_strlcpy(sub_s, s, len + 1);
+	return (sub_s);
+}
+
+int	split_count(char const *s, char c)
+{
+	unsigned int	i;
+	unsigned int	count;
+	
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c && s[i + 1] != c && s[i + 1] != 0)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*s_copy;
+
+	s_copy = (char *) s;
+	while (*s_copy)
+	{
+		if (*s_copy == c)
+			return (s_copy);
+		s_copy++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	unsigned int	s_start;
+	unsigned int	s_final;
+	unsigned int	s_len;
+
+	s_start = 0;
+	s_final = 0;
+	s_len = ft_strlen(s1);
+	while (ft_strchr(set, s1[s_start]))
+		s_start++;
+	s_final += s_len - 1;
+	while (ft_strchr(set, s1[s_final]))
+		s_final--;
+	s_final = (s_len - 1) - s_final;
+	return (ft_substr(s1, s_start, s_len - (s_start + s_final)));
+}
+
+char	**ft_split(char const *s, char c)
+{
+	unsigned int	s_count;
+	unsigned int	s_start;
+	unsigned int 	i;
+	unsigned int	j;
+	char			**split;
+		
+	s_count = split_count(s, c);
+	s_start = 0;
+	i = 0;
+	j = 0;
+	split = (char **)malloc(sizeof(char *) * s_count + 1);
+
+	s = ft_strtrim(s, &c);
+	while (s[i])
+	{
+		if ((s[i] == c && s[i + 1] != c) || s[i + 1] == 0)
+		{
+			split[j] = ft_substr(s, s_start, i - s_start);
+			s_start = i + 1;
+			j++;
+		}
+		i++;
+	}
+	return (split);
+}
+
+// test ft_split
+int	main(void)
+{
+	char *s = "phepllopwoprldp";
+	char c = 'p';
+	char **split = ft_split(s, c);
+	//print **split
+	while (*split)
+	{
+		printf("%s\n", *split);
+		split++;
+	} 
+	return (0);
+}
