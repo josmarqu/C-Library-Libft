13b3a535b715d11f6ee7963e1657a51c9c7f24f6
Jose Marquez montoro <josmarqu@c1r14s5.42madrid.com>
2023-02-05 18:30:45 +0100

    norminette ok

--- functions/ft_memset.c
+++ functions/ft_memset.c
@@ -3,22 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: josmarqu <josmarqu@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: josmarqu <josmarqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 09:23:52 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/02/01 09:24:06 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/02/05 18:29:55 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *s, int c, int n)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*s_copy;
+	unsigned char	*b_copy;
 
-	s_copy = s;
-	while (n--)
-	{
-		*s_copy = c;
-		s_copy++;
-	}
-	return (s);
+	b_copy = b;
+	while (len--)
+		*b_copy++ = (char) c;
+	return (b);
 }
