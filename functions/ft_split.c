b66f5f90a2cd03db07932481b0d060b832150e42
Jose Marquez <josemarquezmontoro2@gmail.com>
2023-02-07 21:22:33 +0100

    fixed some bugs

--- functions/ft_split.c
+++ functions/ft_split.c
@@ -6,48 +6,85 @@
 /*   By: josmarqu <josmarqu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 15:23:15 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/02/06 18:47:56 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/02/07 21:21:43 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-static char	**init_split(char const *s, char c)
+static int	split_count(char const *s, char c)
 {
-	char	**split;
+	unsigned int	i;
+	unsigned int	count;
 
-	split = (char **)malloc(sizeof(char *) * split_count(s, c) + 1);
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c && s[i - 1] != c)
+			count++;
+		i++;
+	}
+	printf("count: %d\n", count);
+	return (count);
+}
+
+static char	**init_split(int count)
+{
+	char			**split;
+
+	split = (char **)malloc(sizeof(char *) * (count + 2));
 	if (split == NULL)
 		return (NULL);
+	return (split);
+}
+
+static char	**split_gen(char **split, char const *s, char c)
+{
+	unsigned int	s_start;
+	unsigned int	i;
+	unsigned int	j;
+
+	s_start = 0;
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if ((s[i] == c && s[i + 1] != c) || (s[i] == 0))
+		{
+			split[j] = (char *)malloc(sizeof(char) * (i - s_start + 1));
+			if (split[j] == NULL)
+				return (NULL);
+			split[j] = ft_substr(s, s_start, i - s_start);
+			s_start = i + 1;
+			j++;
+		}
+		i++;
+	}
+	split[j] = NULL;
 	return (split);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char			**split;
-	unsigned int	s_start;
-	unsigned int	i;
-	unsigned int	j;
+	unsigned int	count;
 
-	if (*s == NULL)
+	if (*s == 0)
 		return (NULL);
 	s = ft_strtrim(s, &c);
-	split = init_split(s, c);
-	s_start = 0;
-	i = 0;
-	j = 0;
-	while (s[i++])
+	if (s == NULL)
+		return (NULL);
+	count = split_count(s, c);
+	split = init_split(count);
+	if (split == NULL)
+		return (NULL);
+	if (count == 0)
 	{
-		if (s[i] == c && s[i - 1] != c)
-		{
-			split[j] = ft_substr(s, s_start, i - s_start);
-			j++;
-			s_start = i + 1;
-		}
-		else if (i >= 1 && s[i - 1] == c)
-			s_start = i;
+		split[0] = (char *)s;
+		split[1] = NULL;
+		return (split);
 	}
-	split[j] = ft_substr(s, s_start, i - s_start);
-	return (split);
+	return (split_gen(split, s, c));
 }
--- functions/ft_strtrim.c
+++ functions/ft_strtrim.c
@@ -6,27 +6,29 @@
 /*   By: josmarqu <josmarqu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 09:19:37 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/02/06 18:50:57 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/02/07 21:21:40 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdlib.h>
+#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	unsigned int	s_start;
-	unsigned int	s_final;
+	unsigned int	s_end;
 	unsigned int	s_len;
 
 	s_start = 0;
-	s_final = 0;
+	s_end = 0;
 	s_len = ft_strlen(s1);
 	while (ft_strchr(set, s1[s_start]))
 		s_start++;
-	s_final += s_len - 1;
-	while (ft_strchr(set, s1[s_final]))
-		s_final--;
-	s_final = (s_len - 1) - s_final;
-	return (ft_substr(s1, s_start, s_len - (s_start + s_final)));
+	if (s_len == s_start)
+		return (ft_substr(s1, 0, 0));
+	s_end += s_len - 1;
+	while (ft_strchr(set, s1[s_end]))
+		s_end--;
+	s_end = (s_len - 1) - s_end;
+	return (ft_substr(s1, s_start, s_len - (s_start + s_end)));
 }
