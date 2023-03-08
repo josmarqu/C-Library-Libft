fc65d56fd30d33ff4cdb57fd269b061ebd205de6
JosePc <josemarquezmontoro@hotmail.com>
2023-03-08 13:06:41 +0100

    free node

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,10 +6,11 @@
 /*   By: josmarqu <josmarqu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 12:58:07 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/03/08 13:01:57 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/03/08 13:06:21 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
@@ -17,4 +18,5 @@ void	ft_lstdelone(t_list *lst, void (*del)(void *))
 	if (lst == NULL)
 		return ;
 	del(lst->content);
+	free(lst);
 }
