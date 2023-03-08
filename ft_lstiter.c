bd454789eac0894e5d564b6dae23856bc76845fd
JosePc <josemarquezmontoro@hotmail.com>
2023-03-08 14:05:49 +0100

    norminette ok

---
+++ ft_lstiter.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josmarqu <josmarqu@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 13:54:02 by josmarqu          #+#    #+#             */
+/*   Updated: 2023/03/08 14:05:34 by josmarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (lst == NULL)
+		return ;
+	while (lst->next != NULL)
+	{
+		f(lst->content);
+	}
+}
