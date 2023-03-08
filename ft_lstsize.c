11901adb1a5f77a0c09b4f141dc1899b3bf302fe
JosePc <josemarquezmontoro@hotmail.com>
2023-03-08 12:21:17 +0100

    unnecesary check deleted

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: josmarqu <josmarqu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 19:28:01 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/03/01 21:10:31 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/03/08 12:09:01 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_lstsize(t_list *lst)
 {
 	unsigned int	i;
 
-	if (lst == NULL || lst->content == NULL)
+	if (lst == NULL)
 		return (0);
 	if (lst->next == NULL)
 		return (1);
