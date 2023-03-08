63008230c355a646a50dccdf2bff753949923b3c
Jose Marquez montoro <josmarqu@c3r3s6.42madrid.com>
2023-03-08 17:21:44 +0100

    function fixed

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,18 +6,18 @@
 /*   By: josmarqu <josmarqu@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 12:23:59 by josmarqu          #+#    #+#             */
-/*   Updated: 2023/03/08 12:42:57 by josmarqu         ###   ########.fr       */
+/*   Updated: 2023/03/08 17:10:04 by josmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
-{
-	t_list	*last;
-
+{	
 	if (lst == NULL)
 		return ;
-	last = ft_lstlast(*lst);
-	last->next = new;
+	if (*lst != NULL)
+		ft_lstlast(*lst)->next = new;
+	else
+		*lst = new;
 }
